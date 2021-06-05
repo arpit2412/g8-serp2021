845d4228875c3bd1441488613a505044eb9d53ef
Fan Zhang <zhangfanfansz@gmail.com>
2021-06-05 23:25:05 +0930

    Modify file path and add progress bar function

--- Poster_replication/Code/ProcessCFilesWithCodeSensor.py
+++ Poster_replication/Code/ProcessCFilesWithCodeSensor.py
@@ -21,29 +21,27 @@ Software_package = "FFmpeg"
 
 Working_directory = "F:\\final year project\\g8-serp2021\\A performance evaluation of deep‐learnt features for software vulnerability detection\\"
 
-# SOURCE_FILE = "\\Vulnerable_Files\\"
+SOURCE_FILE = "Data\\1\\"
 
-SOURCE_FILE = "Data\\Non-vulnerable_funcs\\"
-
-CodeSensor_OUTPUT_PATH = Working_directory + SOURCE_FILE + "Processed_data_results_"+Software_package+"\\"
+CodeSensor_OUTPUT_PATH = Working_directory + SOURCE_FILE + " "+"\\"
 
 CodeSensor_PATH = "F:\\final year project\\g8-serp2021\\codesensor tool\\CodeSensor.jar"
 
 PATH = Working_directory + SOURCE_FILE + Software_package
 
 Full_path = ""
-"F:\final year project\g8-serp2021\A performance evaluation of deep‐learnt features for software vulnerability detection\Data\Non-vulnerable_funcs\Processed_data_results_FFmpeg"
-print(PATH)
+i = 0
 for fpathe, dirs, fs in os.walk(PATH):
-
     for f in fs:
-
-        if (os.path.splitext(f)[1] == '.c'):  # Get the .c files only
+        if (os.path.splitext(f)[1] == '.txt' or os.path.splitext(f)[1] == '.c'):  # Get the .c files only
             file_path = os.path.join(fpathe, f)  # f is the .c file, which will be processed by CodeSensor
 
             # With each .c file open, CodeSensor will process the opened file and output all the processed files to a specified directory.
             Full_path = CodeSensor_OUTPUT_PATH + "_" + f + ".txt"
-            print(Full_path)
+            i=i+1
+            percent=i/len([lists for lists in os.listdir(PATH) if os.path.isfile(os.path.join(PATH, lists))])
+            percent="%.2f%%" % (percent * 100)
+            print("\rCurrent progress:{}".format(percent), end="")
             with open(Full_path, "w+") as output_file:
                 Popen(["java", '-jar', CodeSensor_PATH,file_path],
                       stdout=output_file, stderr=STDOUT)
